bad46e131fc971bdae19f1ef1f439ef4d2b57aaf
oscar-pro <144076286+oscar-pro@users.noreply.github.com>
2026-05-01 21:12:52 -0500

    Actualización 1.1, se mejoro el layout responsive, falta main y footer

--- Public/Javascript/Login.js
+++ Public/Javascript/Login.js
@@ -1,6 +1,19 @@
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded', () => {
+    // Capturamos Dom 
     let btnBack = document.getElementById('Reverse');
-    btnBack.addEventListener('click',()=>{
+    const Ir_Registro = document.getElementById('eXtra');
+    const Registro = document.getElementById('registro-container')
+    const Login = document.getElementById('login-container')
+    // button back 
+    
+    Registro.style.display = 'none'
+
+    btnBack.addEventListener('click', () => {
         window.history.go(-1);
+    });
+
+    Ir_Registro.addEventListener('click', () => {
+        Registro.style.display = 'block';
+        Login.style.display = 'none';
     });
 });
